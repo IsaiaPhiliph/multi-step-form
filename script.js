6aeb43972c47629ef077b44ee9efcc914714f544
IsaiaPhiliph <pablovalverdellamas@gmail.com>
2021-05-23 17:03:45 +0200

    Reworked stylesheet to use CSS variables for colors and form width, also reworked JS to make it adapt to any number of steps

---
+++ script.js
@@ -0,0 +1,147 @@
+initMultiStepForm();
+
+function initMultiStepForm() {
+    const progressNumber = document.querySelectorAll(".step").length;
+    const slidePage = document.querySelector(".slide-page");
+    const submitBtn = document.querySelector(".submit");
+    const progressText = document.querySelectorAll(".step p");
+    const progressCheck = document.querySelectorAll(".step .check");
+    const bullet = document.querySelectorAll(".step .bullet");
+    const pages = document.querySelectorAll(".page");
+    const nextButtons = document.querySelectorAll(".next");
+    const prevButtons = document.querySelectorAll(".prev");
+    const stepsNumber = pages.length;
+
+    if (progressNumber !== stepsNumber) {
+        console.warn(
+            "Error, number of steps in progress bar do not match number of pages"
+        );
+    }
+
+    document.documentElement.style.setProperty("--stepNumber", stepsNumber);
+
+    let current = 1;
+
+    for (let i = 0; i < nextButtons.length; i++) {
+        nextButtons[i].addEventListener("click", function (event) {
+            event.preventDefault();
+
+            inputsValid = validateInputs(this);
+            // inputsValid = true;
+
+            if (inputsValid) {
+                slidePage.style.marginLeft = `-${
+                    (100 / stepsNumber) * current
+                }%`;
+                bullet[current - 1].classList.add("active");
+                progressCheck[current - 1].classList.add("active");
+                progressText[current - 1].classList.add("active");
+                current += 1;
+            }
+        });
+    }
+
+    for (let i = 0; i < prevButtons.length; i++) {
+        prevButtons[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            slidePage.style.marginLeft = `-${
+                (100 / stepsNumber) * (current - 2)
+            }%`;
+            bullet[current - 2].classList.remove("active");
+            progressCheck[current - 2].classList.remove("active");
+            progressText[current - 2].classList.remove("active");
+            current -= 1;
+        });
+    }
+    submitBtn.addEventListener("click", function () {
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+        setTimeout(function () {
+            alert("Your Form Successfully Signed up");
+            location.reload();
+        }, 800);
+    });
+
+    function validateInputs(ths) {
+        let inputsValid = true;
+
+        const inputs =
+            ths.parentElement.parentElement.querySelectorAll("input");
+        for (let i = 0; i < inputs.length; i++) {
+            const valid = inputs[i].checkValidity();
+            if (!valid) {
+                inputsValid = false;
+                inputs[i].classList.add("invalid-input");
+            } else {
+                inputs[i].classList.remove("invalid-input");
+            }
+        }
+        return inputsValid;
+    }
+}
+// nextBtnFirst.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     let inputsValid = true;
+
+//     const inputs = this.parentElement.parentElement.querySelectorAll("input");
+//     for (let i = 0; i < inputs.length; i++) {
+//         const valid = inputs[i].checkValidity();
+//         if (!valid) {
+//             inputsValid = false;
+//             inputs[i].classList.add("invalid-input");
+//         } else {
+//             inputs[i].classList.remove("invalid-input");
+//         }
+//     }
+//     if (inputsValid) {
+//         slidePage.style.marginLeft = `-${(100 / stepsNumber) * current}%`;
+//         bullet[current - 1].classList.add("active");
+//         progressCheck[current - 1].classList.add("active");
+//         progressText[current - 1].classList.add("active");
+//         current += 1;
+//     }
+// });
+// nextBtnSec.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     slidePage.style.marginLeft = `-${(100 / stepsNumber) * current}%`;
+//     bullet[current - 1].classList.add("active");
+//     progressCheck[current - 1].classList.add("active");
+//     progressText[current - 1].classList.add("active");
+//     current += 1;
+// });
+// nextBtnThird.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     slidePage.style.marginLeft = `-${(100 / stepsNumber) * current}%`;
+//     bullet[current - 1].classList.add("active");
+//     progressCheck[current - 1].classList.add("active");
+//     progressText[current - 1].classList.add("active");
+//     current += 1;
+// });
+
+// prevBtnSec.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     slidePage.style.marginLeft = `-${(100 / stepsNumber) * (current - 2)}%`;
+//     bullet[current - 2].classList.remove("active");
+//     progressCheck[current - 2].classList.remove("active");
+//     progressText[current - 2].classList.remove("active");
+//     current -= 1;
+// });
+// prevBtnThird.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     slidePage.style.marginLeft = `-${(100 / stepsNumber) * (current - 2)}%`;
+//     bullet[current - 2].classList.remove("active");
+//     progressCheck[current - 2].classList.remove("active");
+//     progressText[current - 2].classList.remove("active");
+//     current -= 1;
+// });
+// prevBtnFourth.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     slidePage.style.marginLeft = `-${(100 / stepsNumber) * (current - 2)}%`;
+//     bullet[current - 2].classList.remove("active");
+//     progressCheck[current - 2].classList.remove("active");
+//     progressText[current - 2].classList.remove("active");
+//     current -= 1;
+// });
